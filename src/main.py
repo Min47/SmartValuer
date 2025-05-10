9958f94b6dc7d32e8013b969a013a1a58e674147
karming-NB\KarMing <ong.karming@datasonic.com.my>
2025-05-11 00:01:50 +0800

    1. Added scraper folder 2. Updated main.py 3. Added some basic scraping code, not functionable yet

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from database import Session, ListingsSample
+from scraper.initial_full_scrape import PropertyGuruInitialScraper
 
-# Create a session
-session = Session()
+if __name__ == '__main__':
+    # Create a session
+    session = Session()
 
-# Fetch all listings
-# ListingsSample.test_listing()
-ListingsSample.fetch_all(session)
-# Close the session
-session.close()
+    # Fetch all listings
+    # ListingsSample.test_listing()
+    ListingsSample.fetch_all(session)
+    # Close the session
+    session.close()
+
+    # Run the initial full scrape
+    url = "https://www.propertyguru.com.sg/property-for-rent"
+    scraper = PropertyGuruInitialScraper.main()
