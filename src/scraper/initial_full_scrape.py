11247f5c56809d4cabb572cc72cf646873a62b68
Min47 <karming2012@gmail.com>
2025-05-16 19:00:52 +0800

    Removed save_to_db from here

--- src/scraper/initial_full_scrape.py
+++ src/scraper/initial_full_scrape.py
@@ -3,15 +3,14 @@ from scraper.scraper_utils import ScraperUtils
 
 class PropertyGuruInitialScraper:
     @staticmethod
-    def run_scraper(mode, unit_type, desired_pages, session):
+    def run_scraper(session, mode, unit_type, desired_pages):
         try:
             # Validate unit_type for "Buy" mode
             if mode == "Buy" and unit_type == -1:
                 raise ValueError("Invalid unit_type '-1' for mode 'Buy'. Please select a valid unit_type.")
 
-            scraper = ScraperUtils(mode=mode, unit_type=unit_type)
+            scraper = ScraperUtils(session=session, mode=mode, unit_type=unit_type)
             scraper.scrape_listings(desired_pages=desired_pages)
-            # scraper.save_to_csv()
-            scraper.save_to_db(session=session)
+            scraper.save_to_csv()
         except Exception as e:
             print(f"❌ Error on run_scraper: {e}")
