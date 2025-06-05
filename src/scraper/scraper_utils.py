27d354cda6af2837f6f4d1d219f434086327e77e
karming-NB\KarMing <ong.karming@datasonic.com.my>
2025-06-05 14:17:02 +0800

    Put inside data folder

--- src/scraper/scraper_utils.py
+++ src/scraper/scraper_utils.py
@@ -151,7 +151,7 @@ class ScraperUtils:
                 except Exception as e:
                     print(f"❌ Error on Page {cur_page}: {e}")
                     # Take a screenshot for debugging
-                    sb.save_screenshot(f"Listings_Error_Page_{cur_page}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png")
+                    sb.save_screenshot(f"data/Listings_Error_Page_{cur_page}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png")
                     # Save the page source for debugging
                     with open(f"data/Error_Page_{cur_page}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.html", "w", encoding="utf-8") as f:
                         f.write(sb.get_page_source())
